9b233f7837812ced63ad39aa94c77801c9a3e916
Antoniofras309 <afras1@student.unipu.hr>
2025-08-04 23:31:22 +0200

    Added my Name

--- apps/platform/src/routes/index.tsx
+++ apps/platform/src/routes/index.tsx
@@ -31,6 +31,7 @@ function Home() {
     <div>
       <div>
         <p>server time: {new Date(serverTime).toISOString()}</p>
+        <div>Antonio</div>
       </div>
     <button
       type="button"
