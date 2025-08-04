4a319b24cd96480330d9526056dd5fbf803898ec
voiys <matej2714@gmail.com>
2025-08-04 21:20:31 +0200

    init tanstack start

---
+++ apps/platform/src/routes/index.tsx
@@ -0,0 +1,47 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { useState } from "react";
+
+
+const getServerTimestamp = createServerFn({
+  method: "GET",
+}).handler(() => {
+  return {
+    serverTime: Date.now()
+  }
+});
+
+const pingServer = createServerFn({ method: "POST" })
+  .handler(() => {
+    return {
+      serverTime: Date.now()
+    }
+  });
+
+export const Route = createFileRoute("/")({
+  component: Home,
+  loader: async () => await getServerTimestamp(),
+});
+
+function Home() {
+  const loaderData = Route.useLoaderData();
+  const [serverTime, setServerTime] = useState(loaderData.serverTime)
+
+  return (
+    <div>
+      <div>
+        <p>server time: {new Date(serverTime).toISOString()}</p>
+      </div>
+    <button
+      type="button"
+      onClick={async () => {
+        const response = await pingServer()
+
+        setServerTime(response.serverTime)
+      }}
+    >
+      get server time
+    </button>
+    </div>
+  );
+}
