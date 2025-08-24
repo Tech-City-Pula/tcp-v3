502c67d39c6e4c1d333f062c756e267d29203e53
voiys <matej2714@gmail.com>
2025-08-24 20:56:44 +0200

    mock login works

--- apps/admin/src/routes/index.tsx
+++ apps/admin/src/routes/index.tsx
@@ -1,34 +1,55 @@
 import { useForm } from '@tanstack/react-form';
 import { createFileRoute } from '@tanstack/react-router';
 import { createServerFn } from '@tanstack/react-start';
+import { toast } from 'sonner';
 import z from 'zod';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
 
 export const Route = createFileRoute('/')({
   component: Home,
 });
 
+const passwordMinLength = 8;
+const passwordMaxLength = 72;
+
 const loginInputSchema = z.object({
-  username: z.string(),
-  password: z.string(),
+  email: z.email({ message: 'Please enter a valid email address.' }),
+  password: z
+    .string()
+    .min(passwordMinLength, { message: `Password must be at least ${passwordMinLength} characters.` })
+    .max(passwordMaxLength, { message: `Password must be at most ${passwordMaxLength} characters.` }),
 });
+
+const adminUsers = [
+  {
+    email: 'matej@admin.com',
+    password: 'StrongPass123',
+  },
+];
 
 const loginAction = createServerFn({ method: 'POST' })
   .validator(loginInputSchema)
   .handler(({ data }) => {
     console.log(data);
+
+    const adminUser = adminUsers.find((user) => user.email === data.email && user.password === data.password);
+
+    if (!adminUser) {
+      throw new Error('User is not admin. Try again with a different user.');
+    }
   });
 
-const defaultLoginInfo: z.infer<typeof loginInputSchema> = { username: '', password: '' };
+const defaultLoginInfo: z.infer<typeof loginInputSchema> = { email: '', password: '' };
 
 function LoginForm() {
   const form = useForm({
     defaultValues: defaultLoginInfo,
     validators: {
-      onSubmit: loginInputSchema,
+      onChange: loginInputSchema,
     },
     async onSubmit(props) {
       await loginAction({ data: props.value });
@@ -40,7 +61,18 @@ function LoginForm() {
       onSubmit={async (e) => {
         e.preventDefault();
 
-        await form.handleSubmit();
+        try {
+          await form.handleSubmit();
+
+          toast.success('Logged in successfully!');
+
+          form.reset();
+        } catch (error) {
+          if (error instanceof Error) {
+            toast.error(error.message);
+          }
+          console.error(error);
+        }
       }}
       className="w-full max-w-xs"
     >
@@ -51,7 +83,7 @@ function LoginForm() {
         </CardHeader>
         <CardContent>
           <form.Field
-            name="username"
+            name="email"
             children={(field) => {
               return (
                 <div className="flex flex-col gap-2">
@@ -64,6 +96,14 @@ function LoginForm() {
                       field.handleChange(e.target.value);
                     }}
                   />
+                  <em
+                    className={cn(
+                      'invisible h-lh text-red-400 text-xs',
+                      field.state.meta.errors.length > 0 && 'visible'
+                    )}
+                  >
+                    {field.state.meta.errors.map((err) => err?.message).join(', ')}
+                  </em>
                 </div>
               );
             }}
@@ -82,6 +122,14 @@ function LoginForm() {
                       field.handleChange(e.target.value);
                     }}
                   />
+                  <em
+                    className={cn(
+                      'invisible h-lh text-red-400 text-xs',
+                      field.state.meta.errors.length > 0 && 'visible'
+                    )}
+                  >
+                    {field.state.meta.errors.map((err) => err?.message).join(', ')}
+                  </em>
                 </div>
               );
             }}
