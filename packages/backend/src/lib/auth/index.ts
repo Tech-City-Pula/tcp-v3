4c69789c5a950b7018779d4ac7ae8907559d7f9a
voiys <matej2714@gmail.com>
2025-08-25 02:16:12 +0200

    add admin plugin and schema

--- packages/backend/src/lib/auth/index.ts
+++ packages/backend/src/lib/auth/index.ts
@@ -1,9 +1,16 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { admin } from 'better-auth/plugins';
 import { db } from '../db/index.ts'; // your drizzle instance
+import { schema } from '../db/schemas/index.ts';
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [admin()],
   database: drizzleAdapter(db, {
     provider: 'pg', // or "mysql", "sqlite"
+    schema,
   }),
 });
