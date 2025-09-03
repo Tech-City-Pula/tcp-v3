21413650c836b7014d5b001bc73784ed5b9acc6f
voiys <matej2714@gmail.com>
2025-09-03 03:28:50 +0200

    fix: change email schema to ensure string type validation

--- apps/admin/src/lib/validation/auth.ts
+++ apps/admin/src/lib/validation/auth.ts
@@ -7,7 +7,6 @@ export const emailMinLength = 8;
 export const emailMaxLength = 256;
 
 export const emailSchema = z
-  .string()
   .email({ message: 'Please enter a valid email address.' })
   .min(emailMinLength, { message: `Email must be at least ${emailMinLength} characters.` })
   .max(emailMaxLength, { message: `Email must be at most ${emailMaxLength} characters.` });
