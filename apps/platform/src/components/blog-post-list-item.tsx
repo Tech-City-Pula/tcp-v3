85923b164fd430c569c26c261f048429cdae91c4
voiys <matej2714@gmail.com>
2025-09-05 22:57:30 +0200

    refactor: update BlogPostListItem styling and add date formatting

--- apps/platform/src/components/blog-post-list-item.tsx
+++ apps/platform/src/components/blog-post-list-item.tsx
@@ -1,6 +1,7 @@
 import type { schema } from '@repo/backend/schema';
 import { Link } from '@tanstack/react-router';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { formatDate } from '@/lib/posts';
 
 type BlogPostListItemProps = {
   blog: typeof schema.blogs.$inferSelect;
@@ -10,7 +11,7 @@ export function BlogPostListItem(props: BlogPostListItemProps) {
   return (
     <Card className="rounded-xl transition-shadow hover:shadow-md">
       <CardHeader>
-        <CardTitle className="text-red-400 transition-colors hover:text-primary">
+        <CardTitle className="text-primary">
           <Link
             params={{
               slug: props.blog.id,
@@ -20,6 +21,7 @@ export function BlogPostListItem(props: BlogPostListItemProps) {
             {props.blog.title}
           </Link>
         </CardTitle>
+        <p className="font-light text-primary/75 text-xs">{formatDate(props.blog.createdAt.toISOString())}</p>
       </CardHeader>
       <CardContent>
         <p className="line-clamp-3 h-full text-muted-foreground">{props.blog.content}</p>
