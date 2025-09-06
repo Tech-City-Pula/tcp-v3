38ed242a809d213cf5bef344680812977fad9072
Neetko <niko.kusanic@gmail.com>
2025-09-06 21:10:01 +0200

    now renders markdows

--- apps/admin/src/components/rich-text-output.tsx
+++ apps/admin/src/components/rich-text-output.tsx
@@ -1,4 +1,5 @@
+import ReactMarkdown from 'react-markdown';
+
 export function RichTextOutput(props: { markdown: string }) {
-  // TODO: render markdown from db here using the `react-markdown` package
-  return <div className="prose">{props.markdown}</div>;
+  return <ReactMarkdown className="prose">{props.markdown}</ReactMarkdown>;
 }
