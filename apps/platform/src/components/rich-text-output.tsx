f2d626b27e864ea53c1fbede420c59f847b14f58
voiys <matej2714@gmail.com>
2025-09-07 21:25:42 +0200

    fix imports

--- apps/platform/src/components/rich-text-output.tsx
+++ apps/platform/src/components/rich-text-output.tsx
@@ -1,5 +1,5 @@
+import { cn } from '@repo/ui/utils';
 import ReactMarkdown from 'react-markdown';
-import { cn } from '@/lib/utils';
 
 export function RichTextOutput(props: { markdown: string; className?: string }) {
   return <ReactMarkdown className={cn('prose', props.className)} children={props.markdown} />;
